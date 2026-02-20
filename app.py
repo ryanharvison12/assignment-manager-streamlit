cabd5ee736e2e732b78b07992912cd785a7511b3
Ryan Harvison <rharv@udel.edu>
2026-02-20 11:57:30 -0500

    Update app UI

--- app.py
+++ app.py
@@ -1,4 +1,40 @@
 import streamlit as st
 
-st.title("Setup Check")
-st.success("Streamlit works in this environment.")
+st.title("Course Management")
+st.header("Course Management Dashboard")
+st.caption("MISY350")
+st.divider()
+
+# Step 2 Define Assignment List (Data Continuity)
+assignment = [
+    {
+        "id": "HW1",
+        "title": "Introduction to Database",
+        "description": "basics of database design",
+        "points": 100,
+        "type": "homework"
+    },
+    {
+        "id": "HW2",
+        "title": "Normalization",
+        "description": "Normalize the table designs",
+        "points": 100,
+        "type": "lab"
+    }
+]
+
+#3. Step 3: Add New Assignment Section (Inputs & Layouts)
+st.subheader("Add New Assignment")
+with st.container(border=True):
+    col1,col2 = st.columns([2,1])
+
+    with col1:
+        with st.container(border=True):
+            st.markdown("### Assignment Details")
+            title = st.text_input("Assignment title", placeholder="homework")
+            description = st.text_input("Assignment Description")
+            points = st.number_input("Points")
+    with col2:
+        st.markdown("**Time and Type**")
+        due_date = st.date_input("Due Date")
+        type = st.radio("type", ["Homework", "Lab"])
